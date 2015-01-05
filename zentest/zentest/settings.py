4203632a7641bbc53941d959addd1339e5f46328
HammadH <hammadsyed9@gmail.com>
2015-01-05 21:28:19 +0400

    adding static_root

--- zentest/zentest/settings.py
+++ zentest/zentest/settings.py
@@ -93,6 +93,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ( 
     os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 TEMPLATE_CONTEXT_PROCESSORS = DEFAULT_SETTINGS.TEMPLATE_CONTEXT_PROCESSORS + (
     "django.core.context_processors.request",)
